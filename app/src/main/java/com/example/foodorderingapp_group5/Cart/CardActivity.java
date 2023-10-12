4fb54375454c347266e48178a54bd019e34f176b
Nguyễn Đức Hiếu <58695704+hieund288@users.noreply.github.com>
2023-10-13 01:24:49 +0700

    a
    a

---
+++ app/src/main/java/com/example/foodorderingapp_group5/Cart/CardActivity.java
@@ -0,0 +1,16 @@
+package com.example.foodorderingapp_group5.Cart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.foodorderingapp_group5.R;
+
+public class CardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card);
+    }
+}
